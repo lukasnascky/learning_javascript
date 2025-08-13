3e9485c594e30a65066757a92e81d521dc14acce
Lucas Nascimento <159586908+lukasnascky@users.noreply.github.com>
2025-08-13 17:18:24 -0300

    Create script.js

---
+++ alura_curso_javascript/modulo4/script.js
@@ -0,0 +1,25 @@
+// console.log("Olá, mundo!")
+
+// para delcarar uma variável:
+// variável - forma antiga
+// var idade1 = 36
+
+// variável - forma atual - usa let a não ser que haja algum motivo específico para não usar
+// let idade2 = 23
+
+// constante - não muda com o tempo
+// const idade3 = 20
+
+let nome = "Ana"
+let idade = 23
+let altura = 1.68
+
+console.log(typeof nome, typeof idade, typeof altura)
+
+let estaChovendo = false
+
+console.log(typeof estaChovendo)
+
+let cidade
+
+console.log(cidade)
